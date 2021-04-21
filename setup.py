1fc6b27cf964f18a17b3070e8f347ed729a6cae9
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2021-04-21 02:43:05 +0200

    Hack to include numpy in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, Extension, find_packages
 
+try:
+    import numpy
+except ImportError:
+    raise ImportError('Numpy is not available.')
+
 
 extensions = [
     Extension("priority_collections.priority_heap", ["priority_collections/priority_heap.pyx"])
@@ -13,12 +18,12 @@ setup(
     author_email='eleftherios.zisis@epfl.ch',
     packages=find_packages(),
     ext_modules=extensions,
+    include_dirs=[numpy.get_include()],
     include_package_data=True,
     use_scm_version=True,
     setup_requires=[
         'setuptools>=18.0',
         'setuptools_scm',
-        'numpy>=1.19',
         'cython'
     ],
     install_requires=[
