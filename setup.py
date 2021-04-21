93025727d6786680018fbf3b726147fad851c9f5
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2021-04-21 02:07:00 +0200

    Do not include c sources

--- setup.py
+++ setup.py
@@ -1,37 +1,9 @@
 from setuptools import setup, Extension, find_packages
 
 
-modules = {
-    "priority_collections.priority_heap": ["priority_collections/priority_heap"]
-}
-
-
-def build_ext_modules(modules):
-
-    # check if cython is available
-    try:
-        from Cython.Build import cythonize
-        has_cython = True
-    except ImportError:
-        has_cython = False
-
-    ext = 'pyx' if has_cython else 'c'
-
-    extensions = [
-        Extension(name, [f'{path}.{ext}' for path in paths]) for name, paths in modules.items()
-    ]
-
-    if has_cython:
-        print('Building .pyx sources using cython')
-        return cythonize(
-            module_list=extensions,
-            compiler_directives={
-                "language_level": 3,
-                "embedsignature": True
-            })
-
-    print('Building .c sources without cython', extensions)
-    return extensions
+extensions = [
+    Extension("priority_collections.priority_heap", ["priority_collections/priority_heap.pyx"])
+]
 
 
 setup(
@@ -44,7 +16,9 @@ setup(
     include_package_data=True,
     use_scm_version=True,
     setup_requires=[
-        'setuptools_scm'
+        'setuptools>=18.0',
+        'setuptools_scm',
+        'cython'
     ],
     install_requires=[
         'numpy>=1.13'
